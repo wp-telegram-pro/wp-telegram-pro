0fd57b82626e353ce56a2c4b1b8ca4832fa3340c
Parsa Kafi <parselearn@gmail.com>
2019-12-24 11:16:56 +0000

    Update string version to 1.9

--- WPTelegramPro.php
+++ WPTelegramPro.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/parsakafi/wp-telegram-pro
  * Description: Integrate WordPress with Telegram
  * Author: Parsa Kafi
- * Version: 1.8
+ * Version: 1.9
  * Author URI: http://parsa.ws
  * Text Domain: wp-telegram-pro
  * WC requires at least: 3.0.0
